0755048f0761f9f7b33e6e8337f52b6d8bb0493f
Giovannikev <giovannikevin884@gmail.com>
2025-12-14 23:32:03 +0300

    [ADD]: implement authentication system with sign in/up forms
    - Add sign in and sign up pages with form components
    - Set up routing with react-router-dom
    - Add supabase integration for authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import "./App.css";
-import { ModeToggle } from "@/components/dark-mode/mode-toggle";
+import SignupPage from "./pages/auth/signUp/page";
+import SignInPage from "./pages/auth/signIn/page";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <ModeToggle />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/signin" element={<SignInPage />} />
+        <Route
+          path="/dashboard"
+          element={<>Bienvenue dans votre dashboard</>}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
